18e396f6671007690855f57d37757957f2ef32c0
stephensajohnson-hash <stephens.a.johnson@gmail.com>
2026-01-16 12:16:49 -0600

    fix build errors - 9

--- src/Components/Bullet/BulletOrchestratorService.cs
+++ src/Components/Bullet/BulletOrchestratorService.cs
@@ -222,10 +222,10 @@ namespace Dashboard.Services
             }
 
             if (source.Type == "task" && removeFromSource && source.Id > 0)
-            {                
+            {
                 var dbSource = await _db.BulletItems
                     .Include(i => i.Todos)
-                    .Include(i => i.DbTaskDetail) 
+                    .Include(i => i.DbTaskDetail)
                     .FirstOrDefaultAsync(i => i.Id == source.Id);
 
                 if (dbSource != null)
@@ -234,7 +234,7 @@ namespace Dashboard.Services
                         .Where(t => t.IsCompleted)
                         .ToList();
 
-                    if (dbSource.DbTaskDetail != null) 
+                    if (dbSource.DbTaskDetail != null)
                     {
                         dbSource.DbTaskDetail.IsCompleted = true;
                     }
