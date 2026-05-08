326032d708ed99882dddada26b268bded221ed73
stephensajohnson-hash <stephens.a.johnson@gmail.com>
2026-05-08 11:23:52 -0500

    rework PPPStore.razor - 10

--- Program.cs
+++ Program.cs
@@ -74,8 +74,6 @@ builder.Services.AddScoped<PtoService>();
 // NETH Services
 builder.Services.AddScoped<NETHStateService>();
 
-builder.Services.AddScoped<PPPStoreService>();
-
 // --- DATABASE REGISTRATION ---
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
